67157c42fcef8cd0c106f4e5626e87d8132750c3
alexedw01 <alexedw01@icloud.com>
2024-02-26 15:13:24 -0800

    updated the mesh handling

--- CarGame.js
+++ CarGame.js
@@ -1,4 +1,5 @@
 import { tiny, defs } from "./objects.js";
+import { Shape_From_File } from './examples/obj-file-demo.js';
 
 const {
   Vector,
@@ -17,107 +18,6 @@ const {
   Texture,
   Scene,
 } = tiny;
-
-export class Shape_From_File extends Shape {                                   // **Shape_From_File** is a versatile standalone Shape that imports
-                                                                               // all its arrays' data from an .obj 3D model file.
-    constructor(filename) {
-        super("position", "normal", "texture_coord");
-        // Begin downloading the mesh. Once that completes, return
-        // control to our parse_into_mesh function.
-        this.load_file(filename);
-    }
-
-    load_file(filename) {                             // Request the external file and wait for it to load.
-        // Failure mode:  Loads an empty shape.
-        return fetch(filename)
-            .then(response => {
-                if (response.ok) return Promise.resolve(response.text())
-                else return Promise.reject(response.status)
-            })
-            .then(obj_file_contents => this.parse_into_mesh(obj_file_contents))
-            .catch(error => {
-                this.copy_onto_graphics_card(this.gl);
-            })
-    }
-
-    parse_into_mesh(data) {                           // Adapted from the "webgl-obj-loader.js" library found online:
-        var verts = [], vertNormals = [], textures = [], unpacked = {};
-
-        unpacked.verts = [];
-        unpacked.norms = [];
-        unpacked.textures = [];
-        unpacked.hashindices = {};
-        unpacked.indices = [];
-        unpacked.index = 0;
-
-        var lines = data.split('\n');
-
-        var VERTEX_RE = /^v\s/;
-        var NORMAL_RE = /^vn\s/;
-        var TEXTURE_RE = /^vt\s/;
-        var FACE_RE = /^f\s/;
-        var WHITESPACE_RE = /\s+/;
-
-        for (var i = 0; i < lines.length; i++) {
-            var line = lines[i].trim();
-            var elements = line.split(WHITESPACE_RE);
-            elements.shift();
-
-            if (VERTEX_RE.test(line)) verts.push.apply(verts, elements);
-            else if (NORMAL_RE.test(line)) vertNormals.push.apply(vertNormals, elements);
-            else if (TEXTURE_RE.test(line)) textures.push.apply(textures, elements);
-            else if (FACE_RE.test(line)) {
-                var quad = false;
-                for (var j = 0, eleLen = elements.length; j < eleLen; j++) {
-                    if (j === 3 && !quad) {
-                        j = 2;
-                        quad = true;
-                    }
-                    if (elements[j] in unpacked.hashindices)
-                        unpacked.indices.push(unpacked.hashindices[elements[j]]);
-                    else {
-                        var vertex = elements[j].split('/');
-
-                        unpacked.verts.push(+verts[(vertex[0] - 1) * 3 + 0]);
-                        unpacked.verts.push(+verts[(vertex[0] - 1) * 3 + 1]);
-                        unpacked.verts.push(+verts[(vertex[0] - 1) * 3 + 2]);
-
-                        if (textures.length) {
-                            unpacked.textures.push(+textures[((vertex[1] - 1) || vertex[0]) * 2 + 0]);
-                            unpacked.textures.push(+textures[((vertex[1] - 1) || vertex[0]) * 2 + 1]);
-                        }
-
-                        unpacked.norms.push(+vertNormals[((vertex[2] - 1) || vertex[0]) * 3 + 0]);
-                        unpacked.norms.push(+vertNormals[((vertex[2] - 1) || vertex[0]) * 3 + 1]);
-                        unpacked.norms.push(+vertNormals[((vertex[2] - 1) || vertex[0]) * 3 + 2]);
-
-                        unpacked.hashindices[elements[j]] = unpacked.index;
-                        unpacked.indices.push(unpacked.index);
-                        unpacked.index += 1;
-                    }
-                    if (j === 3 && quad) unpacked.indices.push(unpacked.hashindices[elements[0]]);
-                }
-            }
-        }
-        {
-            const {verts, norms, textures} = unpacked;
-            for (var j = 0; j < verts.length / 3; j++) {
-                this.arrays.position.push(vec3(verts[3 * j], verts[3 * j + 1], verts[3 * j + 2]));
-                this.arrays.normal.push(vec3(norms[3 * j], norms[3 * j + 1], norms[3 * j + 2]));
-                this.arrays.texture_coord.push(vec(textures[2 * j], textures[2 * j + 1]));
-            }
-            this.indices = unpacked.indices;
-        }
-        this.normalize_positions(false);
-        this.ready = true;
-    }
-
-    draw(context, program_state, model_transform, material) {               // draw(): Same as always for shapes, but cancel all
-        // attempts to draw the shape before it loads:
-        if (this.ready)
-            super.draw(context, program_state, model_transform, material);
-    }
-}
 
 export class CarGame extends Scene {
   constructor() {
@@ -142,11 +42,9 @@ export class CarGame extends Scene {
         diffusivity: 0.6,
         color: hex_color("#ffffff"),
       }),
-      car: new Material(new defs.Phong_Shader(), {
-        color: hex_color("#000000"),
-        ambient: 1,
-        //texture: new Texture("assets/10600_RC_Car_SG_v1_diffuse.jpg"),
-          texture: new Texture("assets/stars.png", "NEAREST"),
+      car: new Material(new defs.Textured_Phong(), {
+          color: color(.5, .5, .5, 1),
+          ambient: .3, diffusivity: .5, specularity: .5, texture: new Texture("assets/stars.png")
       }),
       road: new Material(new defs.Phong_Shader(), {
         color: hex_color("#D3D3D3"),
